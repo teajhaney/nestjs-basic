dc733f7aa00b92af15f02ec00a3ac10b690e8901
Yusuf <teajhaney@gmail.com>
2025-08-25 09:40:51 +0100

    feat: remove blog module and related files to streamline application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BlogModule } from './blog/blog.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -12,7 +11,7 @@ import { DestinationsModule } from './destinations/destinations.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    BlogModule,
+
     PrismaModule,
     AuthModule,
     DestinationsModule,
